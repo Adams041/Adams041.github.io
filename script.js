130dbec13a78d9e0c453eff15b4d88e58b1354ef
Adams041 <Adamsmuga009@gmail.com>
2025-06-25 12:14:08 +0300

    Update script.js
    Added button functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Welcome to Adams041's portfolio site!");
+// Show/hide button based on scroll position
+  window.onscroll = function () {
+    const button = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  };
+
+  // Scroll to top on click
+  document.getElementById("scrollToTop").onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
